8b2550062f4eecd8e92c653503d5b8175917729b
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2025-02-18 11:29:27 +0200

    Filter fur b subpunkt

---
+++ src/KonfrontationAnalysis.java
@@ -0,0 +1,19 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class KonfrontationAnalysis {
+    private List<Konfrotation> cases;
+
+    public KonfrontationAnalysis(List<Konfrotation> cases) {
+        this.cases = cases;
+    }
+    public void filterByCapacity(int minCapacity) {
+        cases.stream()
+                .filter(spiel -> spiel.getGlobalerEinfluss() >= minCapacity)
+                .forEach(System.out::println);
+    }
+
+}
